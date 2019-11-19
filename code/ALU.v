3f100f59aa319a9c0bbf8e3d895201ca0dc339c8
B06902101 <b06902101@csie.ntu.edu.tw>
2019-11-19 14:57:41 +0800

    Complete ALU Control

---
+++ code/ALU.v
@@ -0,0 +1,11 @@
+/*
+and: 001
+or: 010
+add: 011
+sub: 100
+mul: 101
+addi: 110
+
+nop: 000
+unused:111
+*/
--- code/ALU_Control.v
+++ code/ALU_Control.v
@@ -1,6 +1,21 @@
 module ALU_Control(funct_i, ALUOp_i, ALU_Ctrl_o);
 
-input [9:0] funct_i;
 input [1:0] ALUOp_i;
+input [9:0] funct_i; // funct7 + funct3
 output reg [2:0] ALU_Ctrl_o;
 
+always@(ALUOp_i or funct_i) begin
+	case(ALUOp_i)
+		2'b01:
+			ALUOp_i <= 3'b110;
+		2'b00:
+			case(funct_i)
+				10'b0000000111: ALUCtrl_o <= 3'b001; // and
+				10'b0000000110: ALUCtrl_o <= 3'b010; // or
+				10'b0000000000: ALUCtrl_o <= 3'b011; // add
+				10'b0100000000: ALUCtrl_o <= 3'b100; // sub
+				10'b0000001000: ALUCtrl_o <= 3'b101; // mul
+			default: ALUCtrl_o <= 3'b000; // nop
+			endcase
+	endcase
+end
